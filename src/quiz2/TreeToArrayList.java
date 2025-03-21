836ab723d3fdafd2c2c9767e54d7fb0e1e1ae959
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-03-21 12:33:50 -0400

    Renames and basic outline

---
+++ src/quiz2/TreeToArrayList.java
@@ -0,0 +1,9 @@
+package quiz2;
+
+import java.util.ArrayList;
+
+public class TreeToArrayList {
+    public ArrayList<Integer> traverse(BinaryTreeNode<Integer> node){
+        return new ArrayList<>();
+    }
+}
---
+++ src/quiz2/TreeToHashMap.java
@@ -0,0 +1,8 @@
+package quiz2;
+
+import java.util.HashMap;
+public class TreeToHashMap {
+    public HashMap<Integer,Integer> traverse(BinaryTreeNode<Integer> node){
+        return new HashMap<>();
+    }
+}
