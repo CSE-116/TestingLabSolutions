a4f15eb194afe2fc14fb2dc006b94c5dadc3676d
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-03-31 02:52:15 -0400

    All methods done

--- src/methods/Methods.java
+++ src/methods/Methods.java
@@ -13,6 +13,8 @@ public class Methods {
             return new LinkedListNode<>(list.get(0), null);
         }
 
+        //I used bubble sort because it's easy and I know how to do it,
+        //but you can use any sorting algorithm you want! :)
         for (int i = 0; i < list.size() - 1; i++) {
             for (int j = 0; j < list.size() - 1 - i; j++) {
                 if (list.get(j) > list.get(j + 1)) {
@@ -34,15 +36,58 @@ public class Methods {
         return head;
     }
 
+
+    public int UniqueElementsInArrayList(ArrayList<Integer> list){
+        if(list.isEmpty()){
+            return 0;
+        }
+
+        ArrayList<Integer> uniqueList = new ArrayList<>();
+
+        for(int i : list){
+            if(!uniqueList.contains(i)){
+                uniqueList.add(i);
+            }
+        }
+        return uniqueList.size();
+    }
+
     public int UniqueValuesInHashMap(HashMap<String, Integer> map){
-        return 0;
+        if(map.isEmpty()){
+            return 0;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(String key : map.keySet()){
+            if(!list.contains(map.get(key))){
+                list.add(map.get(key));
+            }
+        }
+        return list.size();
     }
 
     public boolean OneNodeLinkedList(LinkedListNode<String> node){
+        if(node == null || node.getNext() == null){
+            return true;
+        }
         return false;
     }
 
     public double AverageOfLinkedList(LinkedListNode<Integer> LLN){
-        return 0.0;
+        if(LLN == null){
+            return 0.0;
+        }
+
+        int sum = 0;
+        int count = 0;
+
+        while(LLN!=null){
+            sum += LLN.getValue();
+            count++;
+            LLN = LLN.getNext();
+        }
+        double avg = (double) sum / count;
+        return avg;
     }
 }
