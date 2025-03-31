b8926b6b6e5f7db9e2622f05d3aa41c635f2ce0d
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-03-31 06:59:14 -0400

    Tests done

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -4,9 +4,7 @@ import methods.*;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -58,23 +56,123 @@ public class Tests {
 
     @Test
     public void testMethod2(){
+        Methods methods = new Methods();
 
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        //TESTING ALL UNIQUE
+        map.put(5, new ArrayList<>(List.of(1,2,3,4,5)));
+        //TESTING EMPTY
+        map.put(0, new ArrayList<>());
+        //TESTING ALL SAME
+        map.put(1, new ArrayList<>(List.of(5,5,5,5,5,5,5,5,5,5,5)));
+        //TESTING MIXED
+        map.put(3,new ArrayList<>(List.of(1,1,1,1,1,1,2,2,2,2,3,3,3,3,2,2,2,1,1,1,1)));
+
+        for(int key : map.keySet()){
+            assertEquals(key, methods.UniqueElementsInArrayList(map.get(key)));
+        }
     }
 
     @Test
     public void testMethod3(){
+        Methods methods = new Methods();
 
+        HashMap<Integer, HashMap<String, Integer>> map = new HashMap<>();
+        //TESTING EMPTY
+        map.put(0,new HashMap<>());
+
+        //TESTING ALL SAME
+        HashMap<String, Integer> map1 = new HashMap<>();
+        map1.put("Hello,", 1);
+        map1.put("how", 1);
+        map1.put("are", 1);
+        map1.put("you", 1);
+        map1.put("doing", 1);
+        map1.put("today,", 1);
+        map1.put("my", 1);
+        map1.put("friend?", 1);
+        map1.put(":D", 1);
+
+        map.put(1,map1);
+
+        //TESTING ALL UNIQUE
+        HashMap<String, Integer> map2 = new HashMap<>();
+        map2.put("how",-1);
+        map2.put("are", 3);
+        map2.put("you", 4);
+        map2.put("doing", 5);
+        map2.put("today,", 6);
+        map2.put("my", 7);
+        map2.put("friend?", 8);
+        map2.put(":D", 9);
+
+        map.put(8, map2);
+
+        //TESTING MIXED
+        HashMap<String, Integer> map3 = new HashMap<>();
+        map3.put("how",-4);
+        map3.put("are", 3);
+        map3.put("you", -4);
+        map3.put("doing", -4);
+        map3.put("today,", 6);
+        map3.put("my", 7);
+        map3.put("friend?", 6);
+        map3.put(":D", -4);
+
+        map.put(4, map3);
+
+        for(int num : map.keySet()){
+            assertEquals(num,methods.UniqueValuesInHashMap(map.get(num)));
+        }
     }
 
     @Test
     public void testMethod4(){
+        Methods methods = new Methods();
 
+        HashMap<LinkedListNode<String>, Boolean> map = new HashMap<>();
+        //EMPTY LL
+        map.put(null, true);
+
+        //ONE NODE LL
+        map.put(new LinkedListNode<>("Hello world", null), true);
+
+        //MORE THAN ONE NODE LLN
+        map.put(new LinkedListNode<>("Hello", new LinkedListNode<>("World", null)), false);
+
+        for(LinkedListNode<String> node : map.keySet()){
+            assertEquals(methods.OneNodeLinkedList(node), map.get(node));
+        }
     }
 
     @Test
     public void testMethod5(){
+        Methods methods = new Methods();
+        HashMap<Double, LinkedListNode<Integer>> map = new HashMap<>();
 
+        //EMPTY LL
+        map.put(0.0,null);
+
+        //ONE NODE
+        map.put(8.0, new LinkedListNode<>(8, null));
+
+        //MULTIPLE NODES (EASY NUMBERS)
+        LinkedListNode<Integer> node1 = new LinkedListNode<>(1, null);
+        LinkedListNode<Integer> node2 = new LinkedListNode<>(2, node1);
+        LinkedListNode<Integer> node3 = new LinkedListNode<>(3, node2);
+        LinkedListNode<Integer> node4 = new LinkedListNode<>(4,node3);
+        LinkedListNode<Integer> node5 = new LinkedListNode<>(5, node4);
+
+        map.put(3.0, node5);
+
+        //UGLy NUMBER TEST
+        map.put(4.1666, new LinkedListNode<>(10, node5));
+
+        //NEGATIVE NUMBERS
+        map.put(2.0, new LinkedListNode<>(-3, node5));
+
+        for(double avg : map.keySet()){
+            assertEquals(avg, methods.AverageOfLinkedList(map.get(avg)),.001);
+        }
     }
-
-
 }
