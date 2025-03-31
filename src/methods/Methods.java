e42d1543f5e874798514780959e2b25c5f76e75a
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-03-31 02:28:06 -0400

    Method 1 done

--- src/methods/Methods.java
+++ src/methods/Methods.java
@@ -5,7 +5,33 @@ import java.util.HashMap;
 
 public class Methods {
     public LinkedListNode<Integer> IncreasingLinkedListNode(ArrayList<Integer> list){
-        return null;
+        if(list.isEmpty()){
+            return null;
+        }
+
+        if(list.size() == 1){
+            return new LinkedListNode<>(list.get(0), null);
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+
+        LinkedListNode<Integer> head = new LinkedListNode<>(list.get(0), null);
+        LinkedListNode<Integer> tail = head;
+
+        for (int i = 1; i < list.size(); i++) {
+            tail.setNext(new LinkedListNode<>(list.get(i), null));
+            tail = tail.getNext();
+        }
+
+        return head;
     }
 
     public int UniqueValuesInHashMap(HashMap<String, Integer> map){
